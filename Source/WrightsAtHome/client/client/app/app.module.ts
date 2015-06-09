24131d52d6b740eac3452e24a542ae355d96c22b
Bruce Wright <bruce@wrightsideup.com>
2015-06-09 07:11:19 -0500

    Working with HotTowel

---
+++ Source/WrightsAtHome/client/client/app/app.module.ts
@@ -0,0 +1,11 @@
+module app {
+    'use strict';
+
+    angular.module('app', [
+        'app.core',
+        'app.widgets',
+        'app.admin',
+        'app.dashboard',
+        'app.layout'
+    ]);
+}
