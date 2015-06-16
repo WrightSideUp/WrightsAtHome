3fabbde8f3dda024ecb4b841cf9957ba17798189
Bruce Wright <bruce@wrightsideup.com>
2015-06-16 11:14:21 -0500

    Updated gitignore to ignore several files

---
+++ Source/Server/WrightsAtHome.Server.Tests.Integration/Utility/SqlAssert.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Linq;
+using WrightsAtHome.Server.DataAccess;
+
+namespace WrightsAtHome.Tests.Integration.Utility
+{
+    public class SqlAssert
+    {
+        public static bool Count(string sql, int expected)
+        {
+            using (var ctx = new AtHomeDbContext())
+            {
+                var value = ctx.Database.SqlQuery<int>(sql).ToList()[0];
+
+                return value == expected;
+            }
+        }
+    }
+}
